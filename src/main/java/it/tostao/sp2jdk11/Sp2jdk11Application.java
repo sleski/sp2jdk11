c023747ab077d4a8f798aee90044f4e6aa10fb33
Slawomir Leski <slawomir.leski@idnow.de>
2018-12-10 21:26:41 +0100

    Initial commit.

---
+++ src/main/java/it/tostao/sp2jdk11/Sp2jdk11Application.java
@@ -0,0 +1,12 @@
+package it.tostao.sp2jdk11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp2jdk11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp2jdk11Application.class, args);
+	}
+}
