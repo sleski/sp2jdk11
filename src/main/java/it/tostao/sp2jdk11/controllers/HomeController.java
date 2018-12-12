aab122c7e585beee6551907da5c0c6dcfa32d463
Slawomir Leski <slawomir.leski@idnow.de>
2018-12-12 22:59:03 +0100

    Added dependency and homecontroller.

--- src/main/java/it/tostao/sp2jdk11/controllers/HomeController.java
+++ src/main/java/it/tostao/sp2jdk11/controllers/HomeController.java
@@ -1,12 +1,15 @@
 package it.tostao.sp2jdk11.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by Slawomir Leski on 11-12-2018.
  */
-
+@RestController
 public class HomeController {
 
-//	@RequestMapping("/")
+	@RequestMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
