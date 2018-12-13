7d74cc78961237a079f438c6172b0eed323d7376
Slawomir Leski <slawomir.leski@idnow.de>
2018-12-13 23:29:42 +0100

    Added simple test.

--- src/test/java/it/tostao/sp2jdk11/controllers/HomeControllerTest.java
+++ src/test/java/it/tostao/sp2jdk11/controllers/HomeControllerTest.java
@@ -1,9 +1,36 @@
 package it.tostao.sp2jdk11.controllers;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 /**
  * Created by Slawomir Leski on 13-12-2018.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HomeControllerTest {
 
+	private MockMvc mockMvc;
 
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.standaloneSetup(new HomeController()).build();
+	}
+
+	@Test
+	public void testSayHelloWorld() throws Exception {
+		this.mockMvc.perform(get("/").accept(MediaType.parseMediaType(MediaType.APPLICATION_JSON_UTF8_VALUE)))
+				.andExpect(status().isOk());
+				//.andExpect(content().contentType("application/json"));
+
+	}
 }
