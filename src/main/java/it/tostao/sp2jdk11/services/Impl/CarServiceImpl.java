efe91609544a750c4bc06e699773520048be3601
Slawomir Leski <slawomir.leski@gmail.com>
2018-12-26 15:03:48 +0100

    Method create() returns id of new car.

--- src/main/java/it/tostao/sp2jdk11/services/Impl/CarServiceImpl.java
+++ src/main/java/it/tostao/sp2jdk11/services/Impl/CarServiceImpl.java
@@ -5,10 +5,14 @@ import it.tostao.sp2jdk11.mapper.CarMapper;
 import it.tostao.sp2jdk11.services.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Service;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -38,12 +42,11 @@ public class CarServiceImpl implements CarService {
 		return counter;
 	}
 
-	//FIXME id is wrong
 	@Override
 	public int create(Car car) {
 		KeyHolder keyHolder = new GeneratedKeyHolder();
 		String query = "Insert into car (brand, model) VALUES (?, ?)";
-		jdbcTemplate.update(query, car.getBrand(), car.getModel(), keyHolder);
+		jdbcTemplate.update(new MyPreparedStatementCreator(query, car), keyHolder);
 		return keyHolder.getKey().intValue();
 	}
 
@@ -51,5 +54,23 @@ public class CarServiceImpl implements CarService {
 	public void update(Car car) {
 		String query = "UPDATE car brand = ? , model = ? where id = ?";
 		jdbcTemplate.update(query, car.getBrand(), car.getModel(), car.getId());
+	}
+
+	private static class MyPreparedStatementCreator implements PreparedStatementCreator {
+		private final String query;
+		private final Car car;
+
+		public MyPreparedStatementCreator(String query, Car car) {
+			this.query = query;
+			this.car = car;
+		}
+
+		@Override
+		public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+			PreparedStatement ps = con.prepareStatement(query, new String[]{"id"});
+			ps.setString(1, car.getBrand());
+			ps.setString(2, car.getModel());
+			return ps;
+		}
 	}
 }
