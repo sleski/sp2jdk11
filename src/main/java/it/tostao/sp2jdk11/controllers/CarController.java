15d2186fb8ede4929155b634e1895e1f7ece5f36
Slawomir Leski <slawomir.leski@gmail.com>
2018-12-28 16:01:12 +0100

    Added Test for carcontroller.

--- src/main/java/it/tostao/sp2jdk11/controllers/CarController.java
+++ src/main/java/it/tostao/sp2jdk11/controllers/CarController.java
@@ -5,7 +5,9 @@ import it.tostao.sp2jdk11.services.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +32,12 @@ public class CarController {
 	public ResponseEntity<List<Car>> list() {
 		List<Car> cars = carsService.all();
 		return new ResponseEntity<>(cars, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	@ResponseBody
+	public ResponseEntity<Car> add(@RequestBody Car car) {
+		carsService.create(car);
+		return new ResponseEntity<>(car, HttpStatus.CREATED);
 	}
 }
---
+++ src/test/java/it/tostao/sp2jdk11/controllers/CarControllerTest.java
@@ -0,0 +1,50 @@
+package it.tostao.sp2jdk11.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.tostao.sp2jdk11.entities.Car;
+import it.tostao.sp2jdk11.services.Impl.CarServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+/**
+ * Created by Slawomir Leski on 28-12-2018.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/sp2jdk11-test.xml"})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CarControllerTest {
+
+	private MockMvc mockMvc;
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Before
+	public void setup() {
+		CarServiceImpl carsService = new CarServiceImpl(jdbcTemplate);
+		this.mockMvc = MockMvcBuilders.standaloneSetup(new CarController(carsService)).build();
+	}
+
+	@Test
+	public void shouldAddNewCar() throws Exception {
+		Car car = new Car("Audi","TT");
+		ObjectMapper objectMapper = new ObjectMapper();
+		String carToSend = objectMapper.writeValueAsString(car);
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/car/add").contentType(MediaType.APPLICATION_JSON_UTF8).content(carToSend)).andReturn();
+		int status = mvcResult.getResponse().getStatus();
+		assertThat(status, is(201));
+	}
+}
